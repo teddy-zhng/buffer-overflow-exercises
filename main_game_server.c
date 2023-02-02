ee8c0f11c6ae9face99895ed041848c9ef192181
Guy Maverick Levin <33753158+guywhataguy@users.noreply.github.com>
2023-02-01 17:41:53 -0800

    simple tcp echo server

--- main_game_server.c
+++ main_game_server.c
@@ -1,177 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <dirent.h>
 #include <sys/types.h>
-#include <stdint.h>
-#include <time.h>
-#include <locale.h>
-#include <langinfo.h>
-#include "helper.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
+#define BUFFER_SIZE 1024
+#define on_error(...) { fprintf(stderr, __VA_ARGS__); fflush(stderr); exit(1); }
 
-typedef struct dirent dirent;
+int main (int argc, char *argv[]) {
+  if (argc < 2) on_error("Usage: %s [port]\n", argv[0]);
 
-#define PORT 5555							//Defines the Port Address
-#define BACKLOG 10							//Defines the Queue size for Listen
+  int port = atoi(argv[1]);
 
+  int server_fd, client_fd, err;
+  struct sockaddr_in server, client;
+  char buf[BUFFER_SIZE];
 
-//Global Var
-int sockfd;	//This is the file descriptor for the servers socket connection
-fd_set connections;	//Creates a list which manages all incoming requests
-//char client_path[BUFSIZ];
-int dirs_to_ignore;
+  server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd < 0) on_error("Could not create socket\n");
 
-void new()
-{
-	//This 'Paragraph' is mundane, merely Establishes a connection.
-	struct sockaddr_in client_addr;
-	int sin_size = sizeof(struct sockaddr_in);							//This is need because only a reference can be passed (accept takes a non-int parameter)
-	int curfd = error(accept(sockfd, (struct sockaddr *) &client_addr, (socklen_t *) &sin_size), "accept");		//Set current fd to the accept client connection
-	printf("Connection Made\n");
-}
+  server.sin_family = AF_INET;
+  server.sin_port = htons(port);
+  server.sin_addr.s_addr = htonl(INADDR_ANY);
 
+  int opt_val = 1;
+  setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt_val, sizeof opt_val);
 
-//Socket Settings and select loop below
-void setup(struct sockaddr_in * addr )
-{
-    //Sets the socket type to Internet
-    addr->sin_family = AF_INET;
+  err = bind(server_fd, (struct sockaddr *) &server, sizeof(server));
+  if (err < 0) on_error("Could not bind socket\n");
 
-    //Sets the port to Network Standard Endianess value of PORT
-    addr->sin_port = htons(PORT);
+  err = listen(server_fd, 128);
+  if (err < 0) on_error("Could not listen on socket\n");
 
-    //Sets the server IP to the current machine's address
-    addr->sin_addr.s_addr = INADDR_ANY;
+  printf("Server is listening on %d\n", port);
 
-    //Zeroes the rest of the struct 
-    memset(addr->sin_zero, 0, 8);
-}
+  while (1) {
+    socklen_t client_len = sizeof(client);
+    client_fd = accept(server_fd, (struct sockaddr *) &client, &client_len);
 
-void socketoptions()
-{
-    int flag = 1;
-    error(setsockopt( sockfd, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(int)), "setsockopt");     //Make sure to put before bind
-}
+    if (client_fd < 0) on_error("Could not establish new connection\n");
 
-void establish(struct sockaddr_in * addr)
-{
-	//Create a Internet Domain Stream Socket
-	sockfd = error(socket(AF_INET, SOCK_STREAM, 0), "socket");
-	
-	//printf("%d\n",addr->sin_port );
+    while (1) {
+      int read = recv(client_fd, buf, BUFFER_SIZE, 0);
 
-	//Puts this computer's address and the specified PORT number into the struct, sets it to AF_INET, and zeroes sin_zero 
-	setup(addr);
+      if (!read) break; // done reading
+      if (read < 0) on_error("Client read failed\n");
 
-	//Set the Options for the socket, particularly Re-Use of the PORT
-	socketoptions();
-
-	//printf("%d\n",addr->sin_port );
-	
-	//Register the socket with the OS
-    error(bind(sockfd, (struct sockaddr *) addr, sizeof(struct sockaddr)), "bind");
-    
-    //Start waiting for incoming signals, and cap the queue at BACKLOG
-    error(listen(sockfd, BACKLOG), "listen" );
-}
-
-void build_select_list()
-{
-    FD_ZERO(&connections);					//Zero all values/clear
-
-    FD_SET(sockfd, &connections);			//Ensures that the Set will listen to the socket.
-    
-    FD_SET(0, &connections);				//Allows Listening on stdin
-}
-
-void initialize()
-{
-	struct sockaddr_in my_addr;
-	
-	//Creates all the settings for the socket    
-    establish(&my_addr);
-
-    printf("The Server is now active on Port: %d \n", PORT);
-
-    char intro[] = "The Server is now Running, to quit at any time, type in 'QUIT' or 'Q'/'q'.\nAs of now all other input will be ignored.\n";
-    error( write(1, intro, strlen(intro)), "write");
-}
-
-int terminate()
-{
-	char temp[BUFSIZ];
-	char * asses = fgets( temp, BUFSIZ, stdin);
-	lower(temp);
-    return ( !asses || !strcmp(temp, "q\n") || !strcmp(temp,"quit\n") );
-    //	return 1;
-    //return 0;
-}
-
-int check()
-{
-	if (FD_ISSET(sockfd, &connections))
-	{
-			new();
-	}
-	else if (FD_ISSET(0, &connections))
-	{
-		printf("Terminating\n");
-		return terminate();
-	}
-	return 0;
-}
-
-void run()
-{
-	build_select_list();	//Can be added to main for line count if needed
-	while( error(select(FD_SETSIZE, &connections, NULL, NULL, NULL), "select") > 0 )		//error(accept(sockfd, (struct sockaddr *) &client_addr, (socklen_t *) &sin_size), "accept")
-	{
-		printf("Slected\n");
-		if( check() )
-			return;
-		build_select_list();	//This may need to come before check
-	}
-}
-
-void disengage()
-{
-	int pid, status;
-	//Shutsdown the socket
-    shutdown(sockfd, SHUT_RDWR);
-    
-    //Terminates/Closes the Socket
-    close(sockfd);
-
-    printf("Waiting for Children\n");
-    //Prevent Zombification
-    while ( ( pid = wait(&status) ) > 0)
-    {
-    	printf("%d completed with status : %d\n",pid, status );
+      err = send(client_fd, buf, read, 0);
+      if (err < 0) on_error("Client write failed\n");
     }
+  }
 
-}
-
-int main()
-{
-	//Prints Inital message and intializes connections
-	initialize();
-
-	setbuf(stdout,0);	//For Debug Purposes
-
-    //Loops through and accepts connections iteratively
-    run();
-
-	//Cleanup
-	disengage();
-
-	return 0;
+  return 0;
 }
