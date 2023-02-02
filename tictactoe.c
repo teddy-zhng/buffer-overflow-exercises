70a5d3f981d698c50ab6816bf514cb352ba5fd81
teddy-zhang <53926281+teddy-zhang@users.noreply.github.com>
2023-02-02 13:18:53 -0800

    minor

--- tictactoe.c
+++ tictactoe.c
@@ -31,12 +31,31 @@ bool handle_place(int client_fd, char* client_str) {
 bool handle_read_board(int client_fd, char* client_str) {
     for (int x = 0; x < __BOARD_SIZE__; x++) {
         for (int y = 0; y < __BOARD_SIZE__; y++) {
-            //send the info at each location
+            //send the info at each location to client
         }
     }
 
     return 0;
 }
+
+//commented function sends the board as a 36-byte (9 ints) char*
+// bool handle_read_board(int client_fd, char* client_str) {
+//     char board_pkt[9];
+//     int i = 0;
+//     for (int x = 0; x < __BOARD_SIZE__; x++) {
+//         for (int y = 0; y < __BOARD_SIZE__; y++) {
+//             board_pkt[i] = board[x][y];
+//             i++;
+//         }
+//     }
+
+//     //send the board_pkt string to client
+
+//     return 0;
+// }
+
+
+
 
 bool handle_get_winner(int client_fd) {
     //implement a more effecient check if needed 
@@ -45,30 +64,18 @@ bool handle_get_winner(int client_fd) {
     
     for (int i = 0; i < __BOARD_SIZE__; i++) {
         //check vertical lines
-        if ((board[i][0] != E) && 
-            (board[i][0] == board[i][1]) && 
-            (board[i][0] == board[i][2]))
-
+        if ((board[i][0] != E) && (board[i][0] == board[i][1]) && (board[i][0] == board[i][2]))
             winner = board[i][0];
 
         // check horizontal lines
-        if ((board[0][i] != E) &&
-            (board[0][i] == board[1][i]) && 
-            (board[0][i] == board[2][i]))
-
+        if ((board[0][i] != E) && (board[0][i] == board[1][i]) && (board[0][i] == board[2][i]))
             winner = board[0][i];
     }
     // check diagonals
-    if ((board[0][0] != E) && 
-        (board[0][0] == board[1][1]) && 
-        (board[0][0] == board[2][2]))
-
+    if ((board[0][0] != E) && (board[0][0] == board[1][1]) && (board[0][0] == board[2][2]))
         winner = board[0][0];
 
-    if ((board[2][0] != E) && 
-        (board[2][0] == board[1][1]) &&
-        (board[0][0] == board[0][2]))
-
+    if ((board[2][0] != E) && (board[2][0] == board[1][1]) && (board[0][0] == board[0][2]))
         winner = board[2][0];
 
     //send winner to db stub code
