8a96e947786a9939e84704fd32354e6a40ae6cff
teddy-zhang <53926281+teddy-zhang@users.noreply.github.com>
2023-02-05 19:14:49 -0800

    added sending/receiving gameplay packets

--- tictactoe.c
+++ tictactoe.c
@@ -16,48 +16,52 @@
 enum Player board[__BOARD_SIZE__][__BOARD_SIZE__] = { //(0, 0) top left, (2, 2) bottom right
     {E, E, E}, 
     {E, E, E}, 
-    {E, E, E}  
+    {E, E, E} 
 };
 
 bool handle_place(int client_fd, char* client_str) {
-    enum Player player = (int)(*client_str); //first four bytes of packet are the player
-    int x_coord = (int)( *( client_str + sizeof(int)) );
-    int y_coord = (int)( *( client_str + sizeof(int) * 2) );
+    enum Player player; //first four bytes of packet are the player
+    while ( (player = get_int_from_client(client_fd)) == -1) { }
+    int x_coord; //next four bytes are the x
+    while ( (x_coord = get_int_from_client(client_fd)) == -1) { }
+    int y_coord; //last 4 are the y
+    while ( (y_coord = get_int_from_client(client_fd)) == -1) { }
+
     board[x_coord][y_coord] = player;
 
-    return 0;
+    return true;
 }
 
 //should we send the whole board as a (9 * 4) byte string instead? modify client/how we send this
-bool handle_read_board(int client_fd, char* client_str) {
-    for (int x = 0; x < __BOARD_SIZE__; x++) {
-        for (int y = 0; y < __BOARD_SIZE__; y++) {
-            //send the info at each location to client
-        }
-    }
-
-    return 0;
-}
-
-//commented function sends the board as a 36-byte (9 ints) char*
 // bool handle_read_board(int client_fd, char* client_str) {
-//     char board_pkt[9];
-//     int i = 0;
 //     for (int x = 0; x < __BOARD_SIZE__; x++) {
 //         for (int y = 0; y < __BOARD_SIZE__; y++) {
-//             board_pkt[i] = board[x][y];
-//             i++;
+//             //send the info at each location to client
 //         }
 //     }
-
-//     //send the board_pkt string to client
 
 //     return 0;
 // }
 
+// sends the board as a 36-byte (9 ints) char*
+bool handle_read_board(int client_fd, char* client_str) {
+    char board_pkt[9];
+    int i = 0;
+    for (int x = 0; x < __BOARD_SIZE__; x++) {
+        for (int y = 0; y < __BOARD_SIZE__; y++) {
+            board_pkt[i] = board[x][y];
+            i++;
+        }
+    }
+
+    respond_str_to_client(client_fd, board_pkt);
+
+    return true;
+}
 
 
-bool handle_get_winner(int client_fd) {
+
+bool handle_get_winner(int client_fd, char* client_str) {
     //implement a more effecient check if needed 
 
     enum Player winner = E;
@@ -78,9 +82,9 @@ bool handle_get_winner(int client_fd) {
     if ((board[2][0] != E) && (board[2][0] == board[1][1]) && (board[0][0] == board[0][2]))
         winner = board[2][0];
 
-    //send winner to db stub code
-    add_winner(client_fd, winner);
+    //send winner to client
+    respond_str_to_client(client_fd, (char*)winner);
 
-    return 0;//what to return for status?
+    return true;//what to return for status?
 
 }
